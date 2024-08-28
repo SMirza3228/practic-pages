a534f00a742c6c137af83d9f306f0ef403471342
SMirza3228 <mirzaseyidov38@gmail.com>
2024-08-28 18:39:17 +0200

    thre commit box

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex'
 };
+
+$(document).ready(function() {
+    $('#show-header').click(function() {
+        $('#header').fadeIn('slow');
+    });
+});
